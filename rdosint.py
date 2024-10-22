96b3b2ecc8338ab78f2fd825c819d0a4a771ee89
Zay Zay <67672136+zayzaymm@users.noreply.github.com>
2024-10-22 11:38:46 +0630

    Update rdosint.py

--- rdosint.py
+++ rdosint.py
@@ -21,7 +21,7 @@ reset = "\033[0m"
 reddit = praw.Reddit(
     client_id='R2M_pIpfNt2W20BeSFtI1g',
     client_secret='uaBeAQBfloSs1A2NOqxx1TpsIL5eVw',
-    user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 7_2_5; like Mac OS X) AppleWebKit/535.17 (KHTML, like Gecko)  Chrome/49.0.2503.271 Mobile Safari/534.9'
+    user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/99.0.4844.47 Mobile/15E148 Safari/604.1'
   )
 print("")
 banner = f"""{blue}
